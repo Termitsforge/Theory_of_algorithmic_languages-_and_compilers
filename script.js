9bde9d90f26f6707b8ca5e0320d65cfe1e683dd9
orehov <orehov@zelax.ru>
2021-09-10 20:05:44 +0300

    11

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
- 
+let inputString = document.querySelector(".input");
+let rpn = document.querySelector(".rpn");
+console.log(inputString.value);
+setInterval(()=>{
+    rpn.textContent = inputString.value;
+},100);
+
+function RPN(string){
+    
+}
